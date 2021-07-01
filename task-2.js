a0d404c706c41ff2da9d3bf94a9e231577e15ebe
88888tata88888 <tashakeks@gmail.com>
2021-07-01 12:37:28 +0300

    правки

---
+++ task-2.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const listContainer = document.querySelector('#ingredients');
+const insertListItems = items => {
+  return items.map(element => {
+    let unit = document.createElement(`li`);
+    unit.textContent = element;
+    return unit;
+  });
+};
+const allUnits = insertListItems(ingredients);
+listContainer.append(...allUnits);
