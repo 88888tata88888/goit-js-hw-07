b7f330abe41bdcd003133701b77282bbfd1ba7a0
88888tata88888 <tashakeks@gmail.com>
2021-07-01 12:43:40 +0300

    правки

--- js/task-2.js
+++ js/task-2.js
@@ -1,44 +1,19 @@
-/*const ingredients = [
-    'Картошка',
-    'Грибы',
-    'Чеснок',
-    'Помидоры',
-    'Зелень',
-    'Приправы',
-  ];
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
 
-  const listContainer = document.querySelector('ul#ingredients');
-  const insertListItems = items => {
-      const unit = document.createElement('li');
-      unit.textContent = items;
-      return unit;
-  };
-
-  const allUnits = ingredients.map(items => insertListItems(items));
-  listContainer.append(...allUnits); */
-
-  const ingredients = [
-    'Картошка',
-    'Грибы',
-    'Чеснок',
-    'Помидоры',
-    'Зелень',
-    'Приправы',
-  ];
-
-  const listContainer = document.querySelector('ul#ingredients');
-  const insertListItems = items => {
-    let array = [];
-    ingredients.forEach(element => {
-      let unit = document.createElement('li');
-      unit.textContent = element;
-      array.push(unit);
-      console.log(unit);
-    });
-     console.log(array.join(''));
-
-      return array;
-  };
-
-  const allUnits = insertListItems(ingredients);
-  listContainer.insertAdjacentHTML('afterbegin', ...allUnits);
+const listContainer = document.querySelector('#ingredients');
+const insertListItems = items => {
+  return items.map(element => {
+    let unit = document.createElement(`li`);
+    unit.textContent = element;
+    return unit;
+  });
+};
+const allUnits = insertListItems(ingredients);
+listContainer.append(...allUnits);
