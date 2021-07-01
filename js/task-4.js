f6066e56f198cb9945399478c049f495520b9162
88888tata88888 <tashakeks@gmail.com>
2021-07-01 12:47:14 +0300

    правки сложные очень

---
+++ js/task-4.js
@@ -0,0 +1,11 @@
+const counterValueEl = document.querySelector('#value');
+
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+
+const incrementFunction = () =>
+  (counterValueEl.textContent = Number(counterValueEl.textContent) + 1);
+const decrementFunction = () =>
+  (counterValueEl.textContent = Number(counterValueEl.textContent) - 1);
+decrementBtn.addEventListener('click', decrementFunction);
+incrementBtn.addEventListener('click', incrementFunction);
