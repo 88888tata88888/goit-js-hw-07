82e56c4ecda4d52f1a844f8ed8e35c8e4ae47ed8
88888tata88888 <tashakeks@gmail.com>
2021-07-06 18:17:14 +0300

    правки

--- js/task-7.js
+++ js/task-7.js
@@ -1,7 +1,5 @@
-const refs = {
-  input: document.querySelector('#font-size-control'),
-  span: document.querySelector('#text')
-};
-refs.input.addEventListener('input', (event) => {
-  refs.span.style.fontSize = `${event.currentTarget.value}px`;
-});
+const spanRef = document.querySelector('span#text');
+const sliderRef = document.querySelector("input#font-size-control");
+const initialFontSize = parseInt(window.getComputedStyle(spanRef).fontSize);
+const initialSliderValue = Number(sliderRef.value)
+sliderRef.addEventListener("input", (event) => spanRef.style.fontSize = `${initialFontSize / initialSliderValue * event.currentTarget.value}px`);
